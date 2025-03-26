0b8307dd6d5b3e386c3bf9c642a627c86620cd9e
lorgu <lorenz_gutscher@yahoo.de>
2025-03-26 12:06:15 +0100

    coordinate conv

--- scripts.js
+++ scripts.js
@@ -50,6 +50,9 @@ window.onload = function() {
     // Convert coordinates to pixels
     const point1Position = latLonToPixels(point1Coords.lat, point1Coords.lon, mapWidth, mapHeight);
     const point2Position = latLonToPixels(point2Coords.lat, point2Coords.lon, mapWidth, mapHeight);
+    const point3Position = latLonToPixels(point3Coords.lat, point3Coords.lon, mapWidth, mapHeight);
+    const point4Position = latLonToPixels(point4Coords.lat, point4Coords.lon, mapWidth, mapHeight);
+    const point5Position = latLonToPixels(point5Coords.lat, point5Coords.lon, mapWidth, mapHeight);
 
     // Set the position of point1 (converted to pixel values)
     point1.style.top = `${point1Position.y}px`;
@@ -58,6 +61,18 @@ window.onload = function() {
     // Set the position of point2 (converted to pixel values)
     point2.style.top = `${point2Position.y}px`;
     point2.style.left = `${point2Position.x}px`;
+    
+    // Set the position of point3 (converted to pixel values)
+    point3.style.top = `${point3Position.y}px`;
+    point3.style.left = `${point3Position.x}px`;
+    
+    // Set the position of point4 (converted to pixel values)
+    point4.style.top = `${point4Position.y}px`;
+    point4.style.left = `${point4Position.x}px`;
+    
+    // Set the position of point5 (converted to pixel values)
+    point5.style.top = `${point5Position.y}px`;
+    point5.style.left = `${point5Position.x}px`;
 };
 
 const audioPlayer = document.getElementById('audio-player');
@@ -65,6 +80,9 @@ const audioPlayer = document.getElementById('audio-player');
 // Add click listeners to the points
 document.getElementById('point1').addEventListener('click', () => playAudio('point1'));
 document.getElementById('point2').addEventListener('click', () => playAudio('point2'));
+document.getElementById('point3').addEventListener('click', () => playAudio('point3'));
+document.getElementById('point4').addEventListener('click', () => playAudio('point4'));
+document.getElementById('point5').addEventListener('click', () => playAudio('point5'));
 
 function playAudio(pointId) {
     const audioSrc = audioFiles[pointId];
