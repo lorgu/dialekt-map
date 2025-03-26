fef1d2b006bb67e0d5666134462042176572c29d
lorgu <lorenz_gutscher@yahoo.de>
2025-03-26 11:18:19 +0100

    show both points

--- scripts.js
+++ scripts.js
@@ -4,6 +4,20 @@ const audioFiles = {
     point2: './assets/D2_Schiff.wav'
 };
 
+window.onload = function() {
+    // Get the points by their IDs
+    var point1 = document.getElementById('point1');
+    var point2 = document.getElementById('point2');
+
+    // Set the position of point1 (top: 48%, left: 55%)
+    point1.style.top = '48%';
+    point1.style.left = '55%';
+
+    // Set the position of point2 (top: 60%, left: 30%)
+    point2.style.top = '60%';
+    point2.style.left = '30%';
+};
+
 const audioPlayer = document.getElementById('audio-player');
 
 // Add click listeners to the points
